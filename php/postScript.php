8bdb68611a8765b8e87bffb7069aea54a970ba06
Daniel Jimenez <danieljimher@hotmail.es>
2018-11-22 00:13:30 -0600

    Modified DateTime

--- php/postScript.php
+++ php/postScript.php
@@ -1,16 +1,6 @@
 <?php
    include("session.php");
    /* Setting Time to UTC */
-    define('TIMEZONE', 'Europe/Paris');
-    date_default_timezone_set(TIMEZONE);
-    $now = new DateTime();
-    $mins = $now->getOffset() / 60;
-    $sgn = ($mins < 0 ? -1 : 1);
-    $mins = abs($mins);
-    $hrs = floor($mins / 60);
-    $mins -= $hrs * 60;
-    $offset = sprintf('%+d:%02d', $hrs*$sgn, $mins);
-    $db->query("SET time_zone='$offset';");
 
    if($_SERVER["REQUEST_METHOD"] == "POST") {
       // username and password sent from form 
