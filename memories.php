9ae1a958e69fd869c0ca1a0d396ab8b94d44d97f
Daniel Jimenez <danieljimher@hotmail.es>
2018-11-25 16:48:10 -0600

    Prueba de input en ios

--- memories.php
+++ memories.php
@@ -206,45 +206,7 @@
 				$('#btnShow').toggle();
 			});
         });
-				</script>
-				<!--Script for image preview upload-->
-				<script type="text/javascript">
-							$(document).ready( function() {
-						$(document).on('change', '.btn-file :file', function() {
-					var input = $(this),
-						label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
-					input.trigger('fileselect', [label]);
-					});
 
-					$('.btn-file :file').on('fileselect', function(event, label) {
-							
-							var input = $(this).parents('.input-group').find(':text'),
-									log = label;
-							
-							if( input.length ) {
-									input.val(log);
-							} else {
-									if( log ) alert(log);
-							}
-						
-					});
-					function readURL(input) {
-							if (input.files && input.files[0]) {
-									var reader = new FileReader();
-									
-									reader.onload = function (e) {
-											$('#img-upload').attr('src', e.target.result);
-									}
-									
-									reader.readAsDataURL(input.files[0]);
-							}
-					}
-
-					$("#fileToUpload").change(function(){
-							readURL(this);
-					}); 	
-				});
-				</script>
 
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.8.1/baguetteBox.min.js"></script>
 		<script>
